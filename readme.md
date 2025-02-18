1c29cb829c6cbf038d9e774c6e6013bcbb2c7dfd
Carlos Hernandez <carlo96prod@gmail.com>
2025-02-17 16:39:54 -0800

    get all drivers by race / read me file

---
+++ readme.md
@@ -0,0 +1,22 @@
+This App is a side project to see F1 simple data using OpenF1 API's and has just for educational propuse, this project don't have a commercial propurse and is open code!
+
+Delimitation:
+
+    1 Connect to the openF1 API ✅
+    2 Brings all the drivers/pilot of specific seasson (Is not posible on an easy way due the stucture of the API, It could be brought per session/Race)
+    3 Show all the info of an specific driver and the record of the last or current seasson (Create fixed data)
+    4 Bring all the info about each circuit metting, just the race session
+    5 In a graph show the position of each driver/pilot in each lap
+
+NOTE: there are some limitation with the api data and the information structure that they provide, also they just provide the seassons 2023(incomplete data) and 2024
+
+
+Consideration:
+I have to create some fixed data that complement the APIs data for create a better experience
+
+    Ex: Data about each team and images about team and cars
+
+Also there are not data about the position of each pilot in each lap, soo I have to treat the pilot position on a diferent way due the APIs just offer the time when they get the new position, for create a graph I have to compared each time of each other pilot.
+
+
+Any disclaimer please contact me.
--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,60 @@
 import {useState, useEffect } from 'react'
-import './style.css'
+import './style.scss'
+
+const API_BASE_URL = 'https://api.openf1.org/v1'
+
+// Endpoint: Brings all the sessions/race of a year
+// https://api.openf1.org/v1/sessions?session_name=Race&year=2024
+
+// Endpoint: Bring all the drivers per session/race
+// https://api.openf1.org/v1/drivers?session_key=9472
+
+// Endpint: Bring all the position based in time for specific session/race
+// https://api.openf1.org/v1/position?session_key=9472
+
+// const TEAM_COLOR = {
+//     'ferrari': 'rgb(255, 0, 0)'
+// }
 
 const App = () => {
+    const [raceSession, setRaceSession] = useState([])
+    const [drivers, setDrivers] = useState([])
 
-    const [driver, setDriver] = useState()
-
+    // NESTED PROMISES ...
     useEffect(()=>{
-        fetch('https://api.openf1.org/v1/drivers?driver_number=16&session_key=9158').then(res => {
+        fetch(`${API_BASE_URL}/sessions?session_name=Race&year=2024`).then(res => {
             return res.json()
         }).then(data => {
-            setDriver(data[0])
-            console.log(data)
+            const session = data.map(s => s.session_key)
+            setRaceSession(session)
+            // fetch(`${API_BASE_URL}/drivers?driver_number=20&session_key=9158`).then(res => {
+            fetch(`${API_BASE_URL}/drivers?session_key=${raceSession[0]}`).then(res => {
+                return res.json()
+            }).then(data => {
+                setDrivers(data)
+                console.log(data)
+            })
         })
     },[])
 
     return (
         <main>
-            <div className='driver-card glass-effect'>
-                {driver && <>
-                    <div>
-                        <h1>{driver.full_name}</h1>
-                        <h2>{driver.team_name}</h2>
-                    </div>
-                    <img src={`${driver.headshot_url.split('png')[0]}png`} alt="driver image" />
-                </>}
-            </div>
+            {drivers.length !== 0 && <>
+                {drivers.map((driver) => {
+                    return(
+                        <div key={driver.driver_number} className='driver-card glass-effect' style={{
+                                // background: `radial-gradient(100.44% 215.01% at 0% 0%, ${TEAM_COLOR[driver.team_name.toLowerCase()] || 'dark-yellow'}, rgb(255, 255, 255))`
+                                background: `radial-gradient(100.44% 215.01% at 0% 0%, #${driver.team_colour}, rgb(255, 255, 255))`
+                            }}>
+                                <div>
+                                    <h1>{driver.full_name}</h1>
+                                    <h2>{driver.team_name}</h2>
+                                </div>
+                                <img src={`${driver.headshot_url.split('png').splice(0, 2).join('png')}png`} alt={`pilot of ${driver.team_name}: ${driver.full_name}`} />
+                        </div>
+                    )
+                })}
+            </>}
         </main>
     )
 }
